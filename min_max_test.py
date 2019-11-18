92f05dcc0392b0e57c704b335eb6bd89a64384b6
chiragGu <chiraggupta199806@gmail.com>
2019-11-18 16:44:34 +0100

    Update min_max_test.py

--- min_max_test.py
+++ min_max_test.py
@@ -7,25 +7,25 @@ Created on Fri Nov 15 10:50:16 2019
 
 import hello
 
-def test_min():
+def test_min1():
     values = (2, 3, 1, 4, 6)
 
     val = hello.min(values)
     assert val == 0
     
-def test_max():
+def test_max1():
     values = (1,2,3,4,5,6)
     
     val = hello.max(values)
     assert val == 6
 
-def test_max():
+def test_max2():
     values = (56,76,45,78)
     
     val = hello.max(values)
     assert val == 78
     
-def test_min():
+def test_min2():
     values = (56,87,100,34,86)
 
     val = hello.min(values)
